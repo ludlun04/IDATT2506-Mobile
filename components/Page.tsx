fa1947c43de096c74eae36a252ab3f32d4112f57
Ludvik Lund-Hole <ludvik.lund.hole@gmail.com>
2025-10-29 11:52:29 +0100

    add button

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { TextInput, TouchableHighlight, View } from 'react-native';
-
+import { Text, TextInput, TouchableHighlight, View } from 'react-native';
+import ListIcon from 'assets/list.svg'
+import { cssInterop } from "nativewind";
 export const Page = () => {
+
+  cssInterop(ListIcon, {
+    className: {
+      target: "style",
+    }
+  });
+
   return (
+
+      
 
       <View className="h-full flex flex-col gap-10 p-10">
         <View className='h-60 bg-green-500'>
 
         </View>
         <View className='flex flex-row gap-10 h-20'>
+          <Text className="text-primary">Bsrdsdfsdddfudh</Text>
           <TextInput className='bg-yellow-500 grow'/>
-          <TouchableHighlight className='w-20 bg-red-500'>
-            <View>
-              
+          <TouchableHighlight>
+            <View className='w-20 h-20 bg-primary rounded-full items-center justify-center'>
+              <ListIcon className='color-on-primary'/>
             </View>
           </TouchableHighlight>
         </View>
--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+config.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer")
+config.resolver.sourceExts = [...config.resolver.sourceExts, "svg"]
+config.resolver.assetExts = config.resolver.assetExts.filter((ext) => ext !== "svg")
 
 module.exports = withNativeWind(config, { input: './global.css' });
